09686555b6ab1c1b2177c77051733047449a1067
zCaio79 <caiomendesfaria@gmail.com>
2025-01-21 17:53:23 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
 
-export default function Home() {
+import { ArrowDown, ArrowRight, } from 'lucide-react';
+
+export default function intro() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="text-white px-6 pt-6 pb-3 gap-4 font-pop flex h-full flex-col w-full justify-center
+     items-center bg-gradient-to-r from-red-500 via-orange-500 to-red-500">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      <main className="flex w-full h-[90vh] items-center justify-around
+       p-6 rounded-lg border-4 border-white border-dashed">
+
+
+        <section className="flex flex-col gap-8 text-8xl p-2 drop-shadow-xl">
+
+          <div className="flex flex-col uppercase font-bold">
+            <h1>Dengue</h1>
+            <h1>Zero</h1>
+          </div>
+
+          <span className="font-mono text-lg font-medium rounded-lg py-2 px-2">
+            Mapa interativo de combate a <span className="font-bold">Dengue</span>
+          </span>
+
+          <div className="flex gap-4 text-lg font-mono font-medium items-center">
+            <a className="bg-white rounded-lg py-1 px-3 text-red-500 hover:text-white hover:bg-opacity-30" href="">Login</a>
+            <a className="rounded-md py-1 px-3 underline" href="#map">Mapear</a>
+          </div>
+
+        </section>
+
+        <article className="flex items-center bg-white bg-opacity-80 border-2 border-white rounded-full p-2">
+
+          <img className=" drop-shadow-xl"
+            src="/mosquito.gif"
+            alt="mosquito gif"
+            width={350}
+            height={200}
+          />
+
+        </article>
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <ArrowDown />
+
+      <section id="about" className="flex flex-col gap-6 w-fit p-8 h-[95vh] rounded-lg bg-red-50">
+
+        <div className='flex flex-row h-fit gap-8 justify-center'>
+
+          <article>
+
+            <img className='bg-red-500 h-full rounded-2xl'
+              src="/sickperson.png"
+              alt="pessoa doente"
+              width={350}
+              height={200}
+            />
+
+          </article>
+
+          <div className='flex flex-col gap-4 w-96 h-fit border-red-500 border-4 border-dashed rounded-lg p-6'>
+
+            <p className='text-base font-mono text-black text-pretty'>A dengue é uma doença febril aguda, sistêmica, dinâmica, debilitante e autolimitada.
+              A maioria dos doentes se recupera, porém, parte deles podem progredir para formas graves, inclusive virem a óbito.
+              A quase totalidade dos óbitos por dengue é evitável e depende, na maioria das vezes, da qualidade da assistência prestada e organização da rede de serviços de saúde.
+            </p>
+
+            <a className="flex gap-2 w-fit font-mono self-end bg-red-500 rounded-lg py-1 px-3 text-white hover:text-red-500 hover:bg-opacity-30" href="https://www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/d/dengue"><ArrowRight/>Saiba mais</a>
+
+          </div>
+
+
+        </div>
+
+        <div>
+          <h1 className='font-mono text-lg bg-red-500 rounded-md p-1 text-center'>Sintomas</h1>
+        </div>
+
+      </section>
+
+      <footer className="flex w-full justify-center">
+        <p className="text-xs font-mono font-extralight"> &#169;{new Date().getFullYear()} Dengue Zero</p>
       </footer>
+
     </div>
   );
 }
